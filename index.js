4b9c63c8f57cc224b930049183ff789fc05c63a0
palSachin2003 <palshachin8090@gmail.com>
2024-02-09 17:38:59 +0530

    update

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
